20e991fc3025f79442b821d8770f807c50a7e4a5
tesvn7 <tesvn7@gmail.com>
2024-03-17 10:36:06 +0530

    0.1

---
+++ src/app/error.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+const Error = () => {
+    return ( 
+        <div>
+            Error-404
+        </div>
+     );
+}
+ 
+export default Error;
---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+'use client'
+
+const NotFound = () => {
+    return ( 
+        <div>
+            <h2>Not Found</h2>
+            <p>Sorry! we'll be right back.</p>
+        </div>
+     );
+}
+ 
+export default NotFound;
